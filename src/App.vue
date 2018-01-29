434a253caf05e1e04087b98bbed0da9f4767f83c
shalldie <394229785@qq.com>
2018-01-29 18:39:24 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+      <vue-qin content="lalalallalalalala"></vue-qin>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+    components: {
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    box-shadow: 0 0 6px #999;
+    padding: 10px;
+    width: 600px;
+    margin: 50px auto;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+import VueQin from './lib';
+Vue.use(VueQin);
+
+import './animate.css';
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
